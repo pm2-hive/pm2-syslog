c9f326f8630bd7b429910d56dda567278fbc0d74
Unitech <as@unitech.io>
2015-03-19 05:12:21 -0400

    push

---
+++ app.js
@@ -0,0 +1,20 @@
+
+process.env.MODULE_DEBUG = (process.NODE_ENV == 'production' ? false : true);
+
+var pmx     = require('pmx');
+var pm2     = require('pm2');
+
+var SysLogger = require('ain2');
+var logger    = new SysLogger({facility: 'syslog'});
+
+var conf    = pmx.initModule();
+
+
+pm2.launchBus(function(err, bus) {
+  bus.on('*', function(event, data){
+    if (event == 'process:event' && data.event == 'online') {
+      logger.log('Process %s restarted %s', data.process.name, data.process.restart_time, function() { console.log(arguments)});
+      console.log('Process %s restarted %s', data.process.name, data.process.restart_time);
+    }
+  });
+});
