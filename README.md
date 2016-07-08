612ff3db9fb0c6b03d00d1d6323a1fecf52fcb55
Unitech <strzelewicz.alexandre@gmail.com>
2016-07-08 11:09:15 +0200

    fix module + better README.md

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # pm2-syslog
 
-Redirect all logs of PM2 + Apps managed into syslog
+Redirect all logs of PM2 + Apps managed into `/var/log/syslog`
+
+## Configure OS
+
+Edit `/etc/rsyslog.conf` and uncomment:
+
+```
+# provides UDP syslog reception
+module(load="imudp")
+input(type="imudp" port="514")
+```
+
+Restart rsyslog:
+
+```
+$ sudo service rsyslog restart
+```
+
+## Install module
 
 ```
 # Install
--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 
-process.env.MODULE_DEBUG = (process.NODE_ENV == 'production' ? false : true);
-
-var pmx     = require('pmx');
-var pm2     = require('pm2');
-
+var pm2       = require('pm2');
 var SysLogger = require('ain2');
-var logger = new SysLogger({tag: 'pm2', facility: 'syslog'});
-
-var conf    = pmx.initModule();
-
+var logger    = new SysLogger({tag: 'pm2',  facility: 'syslog'});
 
 pm2.launchBus(function(err, bus) {
   bus.on('*', function(event, data){
-    if (event == 'process:event' && data.event == 'online') {
-      logger.log('Process %s restarted %s', data.process.name, data.process.restart_time, function() { console.log(arguments)});
-      console.log('Process %s restarted %s', data.process.name, data.process.restart_time);
+    if (event == 'process:event') {
+      logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
+                  data.process.name,
+                  data.process.pm_id,
+                  data.process.restart_time,
+                  data.event);
     }
+  });
+
+  bus.on('log:err', function(data) {
+    logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+  });
+
+  bus.on('log:out', function(data) {
+    logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
   });
 });
