0b4fb31c6a7837462cac27630e439f5f683a4652
Adrian Wilkins <adrian.wilkins@dwp.gsi.gov.uk>
2017-10-30 16:11:22 +0000

    Make it configurable

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-
+var pmx       = require('pmx');
 var pm2       = require('pm2');
 var SysLogger = require('ain2');
-var logger    = new SysLogger({
-  tag: 'pm2',
-  facility: 'syslog',
-  path: '/dev/log'
-});
 
-pm2.launchBus(function(err, bus) {
-  bus.on('*', function(event, data){
-    if (event == 'process:event') {
-      logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
-                  data.process.name,
-                  data.process.pm_id,
-                  data.process.restart_time,
-                  data.event);
-    }
-  });
+var conf      = pmx.initModule({}, (err, conf) => {
+  var logger    = new SysLogger(conf);
 
-  bus.on('log:err', function(data) {
-    logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
-  });
+  pm2.launchBus(function(err, bus) {
+    bus.on('*', function(event, data){
+      if (event == 'process:event') {
+        logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
+                    data.process.name,
+                    data.process.pm_id,
+                    data.process.restart_time,
+                    data.event);
+      }
+    });
 
-  bus.on('log:out', function(data) {
-    logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+    bus.on('log:err', function(data) {
+      logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+    });
+
+    bus.on('log:out', function(data) {
+      logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+    });
   });
 });
