961d8f3966d13a1192dabf5fc7cfd1890c31740b
agrosjean <grosjean.arnaud@gmail.com>
2019-05-18 20:12:24 -0500

    rearanged a little bit the format

--- app.js
+++ app.js
@@ -9,9 +9,17 @@ const conf = pmx.initModule();
 var serverAddress =  conf.serverAddress || "localhost"
 var serverPort =   conf.serverPort || "514"
 console.log(`Starting pm2-syslogger logging to ${serverAddress}:${serverPort}`)
-var logger    = new SysLogger({tag: 'pm2',  facility: 'projectName',address : serverAddress});
+var logger    = new SysLogger({facility: 1 ,address : serverAddress});
  
 logger.setPort(serverPort)
+logger.setHostname("MF-PC")
+
+//format <facility> DATE HOSTNAME APPNAME PID MSGID MSG
+
+logger.setMessageComposer(function(message, severity){
+  return new Buffer('<' + (this.facility * 8 + severity) + '>' +
+          this.getDate() + ' ' + this.hostname + ' ' + message);
+});
 
 pm2.launchBus(function(err, bus) {
   bus.on('*', function(event, data){
@@ -29,6 +37,8 @@ pm2.launchBus(function(err, bus) {
   });
 
   bus.on('log:out', function(data) {
-    logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
-  });
+    message = data.data;
+    logger.log(data.process.name + ' ' + "pid" + ' ' + 'msgID' + ' ' + message)
+    //logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+  })
 });
